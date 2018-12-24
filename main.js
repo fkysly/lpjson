b175d07afd91232d32b0ae13fd78f09ad68c417d
马天翼 <fkysly@gmail.com>
2018-12-25 00:47:41 +0800

    disable devtool

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function createWindow() {
   if (win) return;
   win = new electron.BrowserWindow({ width: 800, height: 600 });
   win.loadURL('file://' + __dirname + '/index.html');
-  win.webContents.openDevTools();
+  // win.webContents.openDevTools();
   win.on('closed', function() {
     win = null;
   });
